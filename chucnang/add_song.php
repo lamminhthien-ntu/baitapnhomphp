096dfa9a2201e1b77848201c809c5404c84358d2
Lâm Minh Thiện - 60136962 <31031585+lamminhthien-ntu@users.noreply.github.com>
2021-10-29 14:57:26 +0700

    re up function validation add_song.php

--- chucnang/add_song.php
+++ chucnang/add_song.php
@@ -7,6 +7,16 @@ date_default_timezone_set("Asia/Ho_Chi_Minh");
 $thoigian = date("d/m/Y");
 
 
+//Lưu lại giá trị để người dùng biết đường xem và sửa cho đến khi form xác nhận hợp lệ chứ!
+if (isset($_POST['title'])) setcookie('title_sticky',$_POST['title'],time()+1,"/");
+if (isset($_POST['loi'])) setcookie('loi_sticky',$_POST['loi'],time()+1,"/");
+if (isset($_POST['singer'])) setcookie('singer_sticky',$_POST['singer'],time()+1,"/");
+
+//Ở đây không khắt khe như add user admin, mình làm đơn giản là check không được bỏ trống.
+if (!isset($_POST['title'])) setcookie('title','Tên bài hát không được trống',time()+1,"/");
+if (!isset($_POST['loi'])) setcookie('loi','Tên bài hát không được trống',time()+1,"/");
+if (!isset($_POST['singer'])) setcookie('singer','Tên ca sĩ không được trống',time()+1,"/");
+
 if(!empty($_POST['title'])&&!empty($_POST['loi'])&&!empty($_POST['singer']))
 {
     $tieude = $_POST['title'];
@@ -37,7 +47,7 @@ if(!empty($_POST['title'])&&!empty($_POST['loi'])&&!empty($_POST['singer']))
         $col_name   = ['tieude','casi','loi','tenfile','ngay_upload','anh_nhac'];
         $col_value  = ["$tieude","$singer","$loi","$name_file_music","$thoigian","$name_file_anh"];
         insert_table($table,$col_name,$col_value);
-        //header('Location: ../admin/index.php');
+        header('Location: ../admin/index.php');
         $_SESSION['message']='<div class="alert alert-success" role="alert">
   Bạn đã thêm bài hát thành công
 </div>';
@@ -48,9 +58,8 @@ if(!empty($_POST['title'])&&!empty($_POST['loi'])&&!empty($_POST['singer']))
         $col_name   = ['tieude','casi','loi','tenfile','ngay_upload'];
         $col_value  = ["$tieude","$singer","$loi","$name_file_music","$thoigian"];
         //insert_table($table,$col_name,$col_value);
-//        var_dump(insert_table($table,$col_name,$col_value));
 //        echo "Test dòng if lúc ko up ảnh nhưng có up nhạc";
-        //header('Location: ../admin/index.php');
+        header('Location: ../admin/index.php');
         $_SESSION['message']='
             <div class="alert alert-success" role="alert">
               Bạn đã thêm bài hát thành công
